34c03124af79a59c69e9cb9b3c10593ddf4471b0
Ruslan2205 <54228532+Ruslan2205@users.noreply.github.com>
2025-11-15 16:26:14 +0300

    merge?844555

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return 'Привет. Это главная страница';
+        return 'Привет.... Это главная страница';
     }
 }
