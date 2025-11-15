deeae691a8cac41866eb6eefd4f778a230d6fedd
Ruslan2205 <54228532+Ruslan2205@users.noreply.github.com>
2025-11-15 15:31:48 +0300

    merge?844

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/book/{id}', [BookController::class, 'show']);
+
