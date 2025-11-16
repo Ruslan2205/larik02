d14cdedf91eed13ee7d347ec60b94f8f4259b319
Ruslan2205 <54228532+Ruslan2205@users.noreply.github.com>
2025-11-16 03:34:31 +0300

    merыыы

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return 'Привет..tgtfggffghghfdg.. Это главная страница';
+        return 'Привет..ва.. Это главная страница';
     }
 }
